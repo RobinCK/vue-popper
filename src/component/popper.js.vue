9e64c3cacce4114b805749302f8a2f87c54112a5
Игорь Огниченко <ognichenko.igor@gmail.com>
2017-05-10 22:24:35 +0300

    add component code

---
+++ src/component/popper.js.vue
@@ -0,0 +1,312 @@
+<style>
+  .popper {
+    width: auto;
+    background-color: #fafafa;
+    color: #212121;
+    text-align: center;
+    padding: 2px;
+    border-radius: 3px;
+    position: absolute;
+    font-size: 14px;
+    font-weight: normal;
+    border: 1px #ebebeb solid;
+    z-index: 200000;
+    -moz-box-shadow: rgb(58, 58, 58) 0 0 6px 0;
+    -webkit-box-shadow: rgb(58, 58, 58) 0 0 6px 0;
+    box-shadow: rgb(58, 58, 58) 0 0 6px 0;
+  }
+
+  .popper .popper__arrow {
+    width: 0;
+    height: 0;
+    border-style: solid;
+    position: absolute;
+    margin: 5px;
+  }
+
+  .popper[x-placement^="top"] {
+    margin-bottom: 5px;
+  }
+
+  .popper[x-placement^="top"] .popper__arrow {
+    border-width: 5px 5px 0 5px;
+    border-color: #fafafa transparent transparent transparent;
+    bottom: -5px;
+    left: calc(50% - 5px);
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  .popper[x-placement^="bottom"] {
+    margin-top: 5px;
+  }
+
+  .popper[x-placement^="bottom"] .popper__arrow {
+    border-width: 0 5px 5px 5px;
+    border-color: transparent transparent #fafafa transparent;
+    top: -5px;
+    left: calc(50% - 5px);
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  .popper[x-placement^="right"] {
+    margin-left: 5px;
+  }
+
+  .popper[x-placement^="right"] .popper__arrow {
+    border-width: 5px 5px 5px 0;
+    border-color: transparent #fafafa transparent transparent;
+    left: -5px;
+    top: calc(50% - 5px);
+    margin-left: 0;
+    margin-right: 0;
+  }
+
+  .popper[x-placement^="left"] {
+    margin-right: 5px;
+  }
+
+  .popper[x-placement^="left"] .popper__arrow {
+    border-width: 5px 0 5px 5px;
+    border-color: transparent transparent transparent #fafafa;
+    right: -5px;
+    top: calc(50% - 5px);
+    margin-left: 0;
+    margin-right: 0;
+  }
+</style>
+
+<template>
+  <span>
+    <transition :name="transition" @after-leave="doDestroy">
+      <span
+        ref="popper"
+        v-show="!disabled && showPopper">
+        <slot>{{ content }}</slot>
+      </span>
+    </transition>
+    <slot name="reference"></slot>
+  </span>
+</template>
+
+<script>
+  import PopperJS from 'popper.js';
+
+  function on(element, event, handler) {
+    if (element && event && handler) {
+      document.addEventListener ? element.addEventListener(event, handler, false) : element.attachEvent('on' + event, handler);
+    }
+  }
+
+  function off(element, event, handler) {
+    if (element && event) {
+      document.removeEventListener ? element.removeEventListener(event, handler, false) : element.detachEvent('on' + event, handler)
+    }
+  }
+
+  export default {
+    props: {
+      trigger: {
+        type: String,
+        default: 'hover',
+        validator: value => ['click', 'hover'].indexOf(value) > -1
+      },
+      disabled: Boolean,
+      content: String,
+      boundariesSelector: String,
+      reference: {},
+      appendToBody: {
+        type: Boolean,
+        default: false
+      },
+      visibleArrow: {
+        type: Boolean,
+        default: true
+      },
+      transition: {
+        type: String,
+        default: 'fade-in-linear'
+      },
+      options: {
+        type: Object,
+        default() {
+          return {};
+        }
+      }
+    },
+
+    data() {
+      return {
+        referenceElm: null,
+        popperJS: null,
+        showPopper: false,
+        currentPlacement: '',
+        popperOptions: {
+          placement: 'bottom',
+          gpuAcceleration: false
+        }
+      };
+    },
+
+    watch: {
+      showPopper(value) {
+        if (value) {
+          this.$emit('show');
+          this.updatePopper();
+        } else {
+          this.$emit('hide');
+        }
+      }
+    },
+
+    created() {
+      this.popperOptions = Object.assign(this.popperOptions, this.options);
+    },
+
+    mounted() {
+      this.referenceElm = this.reference || this.$slots.reference[0].elm;
+      this.popper = this.$slots.default[0].elm;
+
+      switch (this.trigger) {
+        case 'click':
+          on(this.referenceElm, 'click', this.doToggle);
+          on(document, 'click', this.handleDocumentClick);
+          break;
+        case 'hover':
+          on(this.referenceElm, 'mouseover', this.onMouseOver);
+          on(this.popper, 'mouseover', this.onMouseOver);
+          on(this.referenceElm, 'mouseout', this.onMouseOut);
+          on(this.popper, 'mouseout', this.onMouseOut);
+          break;
+      }
+
+      this.createPopper();
+    },
+
+    methods: {
+      doToggle() {
+        this.showPopper = !this.showPopper;
+      },
+
+      doShow() {
+        this.showPopper = true;
+      },
+
+      doClose() {
+        this.showPopper = false;
+      },
+
+      doDestroy() {
+        if (this.showPopper || !this.popperJS) {
+          return;
+        }
+
+        this.popperJS.destroy();
+        this.popperJS = null;
+      },
+
+      createPopper() {
+        this.$nextTick(() => {
+          if (this.visibleArrow) {
+            this.appendArrow(this.popper);
+          }
+
+          if (this.appendToBody) {
+            document.body.appendChild(this.popper)
+          }
+
+          if (this.popperJS && this.popperJS.destroy) {
+            this.popperJS.destroy();
+          }
+
+          if (this.boundariesSelector) {
+            const boundariesElement = document.querySelector(this.boundariesSelector);
+
+            if (boundariesElement) {
+              this.popperOptions.modifiers = Object.assign({}, this.popperOptions.modifiers);
+              this.popperOptions.modifiers.preventOverflow = Object.assign({}, this.popperOptions.modifiers.preventOverflow);
+              this.popperOptions.modifiers.preventOverflow.boundariesElement = boundariesElement;
+            }
+          }
+
+          this.popperOptions.onCreate = () => {
+            this.$emit('created', this);
+            this.$nextTick(this.updatePopper);
+          };
+
+          this.popperJS = new PopperJS(this.referenceElm, this.popper, this.popperOptions);
+        });
+      },
+
+      destroyPopper() {
+        off(this.referenceElm, 'click', this.doToggle);
+        off(this.referenceElm, 'mouseup', this.doClose);
+        off(this.referenceElm, 'mousedown', this.doShow);
+        off(this.referenceElm, 'focus', this.doShow);
+        off(this.referenceElm, 'blur', this.doClose);
+        off(this.referenceElm, 'mouseout', this.onMouseOut);
+        off(this.referenceElm, 'mouseover', this.onMouseOver);
+        off(document, 'click', this.handleDocumentClick);
+
+        this.popperJS = null;
+      },
+
+      appendArrow(element) {
+        let hash;
+
+        if (this.appended) {
+          return;
+        }
+
+        this.appended = true;
+
+        for (const item of element.attributes) {
+          if (/^_v-/.test(item.name)) {
+            hash = item.name;
+            break;
+          }
+        }
+
+        const arrow = document.createElement('div');
+
+        if (hash) {
+          arrow.setAttribute(hash, '');
+        }
+        arrow.setAttribute('x-arrow', '');
+        arrow.className = 'popper__arrow';
+        element.appendChild(arrow);
+      },
+
+      updatePopper() {
+        this.popperJS ? this.popperJS.update() : this.createPopper();
+      },
+
+      onMouseOver() {
+        this.showPopper = true;
+        clearTimeout(this._timer);
+      },
+
+      onMouseOut() {
+        this._timer = setTimeout(() => {
+          this.showPopper = false;
+        }, 10);
+      },
+
+      handleDocumentClick(e) {
+        if (!this.$el || !this.referenceElm ||
+          this.$el.contains(e.target) ||
+          this.referenceElm.contains(e.target) ||
+          !this.popper || this.popper.contains(e.target)) {
+          return;
+        }
+
+        this.showPopper = false;
+      }
+    },
+
+    destroyed() {
+      this.destroyPopper();
+    }
+  }
+</script>
