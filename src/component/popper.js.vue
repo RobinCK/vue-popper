deee6dc2f2a4036e981aa4df282cc23f02372d70
Peter Siska <pesche@gridonic.ch>
2018-03-14 12:32:24 +0100

    Fix codeclimate issue, use a wrapper function for contains()

--- src/component/popper.js.vue
+++ src/component/popper.js.vue
@@ -314,10 +314,9 @@
 
       handleDocumentClick(e) {
         if (!this.$el || !this.referenceElm ||
-          (typeof this.$el.contains === 'function' && this.$el.contains(e.target)) ||
-          (typeof this.referenceElm.contains === 'function' && this.referenceElm.contains(e.target)) ||
-          !this.popper ||
-          (typeof this.popper.contains === 'function' && this.popper.contains(e.target))
+            this.elementContains(this.$el, e.target) ||
+            this.elementContains(this.referenceElm, e.target) ||
+          !this.popper || this.elementContains(this.popper, e.target)
         ) {
           return;
         }
@@ -329,6 +328,14 @@
         }
 
         this.showPopper = false;
+      },
+
+      elementContains(elm, otherElm) {
+        if (typeof elm.contains === 'function') {
+          return elm.contains(otherElm);
+        }
+
+        return false;
       }
     },
 
